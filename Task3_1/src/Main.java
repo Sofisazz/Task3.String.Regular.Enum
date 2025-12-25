8e0af2e59cef662692f0152adbeb1ec1272c78f0
Sofia <gurskayasofia7@gmail.com>
2025-12-25 20:10:58 +0300

    feat: add files task3_1

---
+++ Task3_1/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String stroka = in.nextLine();
+
+        int count = 0;
+
+        Pattern pattern = Pattern.compile("[^\\s\\wА-я\\d]");
+        Matcher matcher = pattern.matcher(stroka);
+        while(matcher.find()){
+            count++;
+        }
+
+        System.out.println("Количество знаков препинания: " + count);
+}
+}
