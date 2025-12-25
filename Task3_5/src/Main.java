4e636b48a5b51c4816f5aa5a76a1e4bc231df8fa
Sofia <gurskayasofia7@gmail.com>
2025-12-25 22:30:51 +0300

    feat: add files task3_5

---
+++ Task3_5/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String stroka = in.nextLine();
+
+        Pattern pattern = Pattern.compile("<p\\s+[^><]*>");
+        Matcher matcher = pattern.matcher(stroka);
+        String newStr = matcher.replaceAll("<p>");
+
+        System.out.println("\nНовая строка:\n" + newStr);
+    }
+}
