d328dacaa1a3697c38552dfc1478fc4207cd49c0
Sofia <gurskayasofia7@gmail.com>
2025-12-25 21:16:47 +0300

    feat: add files task3_2

---
+++ Task3_2/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите текст: ");
+        String text = in.nextLine();
+
+        if (text.isEmpty()) return;
+
+        StringBuilder stroka = new StringBuilder();
+        String[] words = text.split("\\s*[\\s,!.;:?]+\\s*");
+        System.out.println(Arrays.toString(words));
+        for(String word : words){
+            stroka.append(word.charAt(word.length()-1));
+        }
+
+        System.out.println("Полученный текст: " + stroka);
+    }
+
+}
